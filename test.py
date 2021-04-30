db231328d9950dc0fd11cbe7f20605983401e097
huamiaozi <345662642@qq.com>
2021-04-30 16:34:08 +0800

    提交代码test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+# coding = utf-8
+# Author:XiaoFei
+# Date：2021/3/10 16:20
+
+print("陈志霏")
+print('''春眠不觉晓，
+处处闻啼鸟,
+夜来风雨声，
+花落知多少。''')
+print("春眠不觉晓，处处闻啼鸟，夜来风雨声，花落知多少。")
+
+name=input("请输入你的用户:")
+pwd=input("请输入你的用户:")
+print(name+"用户欢迎登录")
+
+a=float(input("请输入第一个数字："))
+b=float(input("请输入第二个数字："))
+print(a,"+",b,"=",a+b)
